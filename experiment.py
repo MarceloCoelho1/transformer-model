1504b250ced4cf2c8112b259f0ea19f17d8566c2
Marcelo <mhbcoelho99@gmail.com>
2025-01-23 11:15:15 -0300

    update experiment

--- experiment.py
+++ experiment.py
@@ -9,37 +9,28 @@ from mtsa.utils import files_train_test_split
 from sklearn import metrics
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 
-device = torch.device("cuda")
 
 
-def run_experiment(model, lr, epoch, batch_size, path):
+def run_experiment(model, lr, epochs, batch_size, path, sampling_rate, mono, shuffle, device):
   torch.cuda.empty_cache()
-
-  model = build_transformer(mfcc_dim=20, d_model=128, nhead=2, num_encoder_layers=2, num_decoder_layers=2,
-                            dim_feedforward=1024, dropout=0.1, batch_first=True, device=device).to(device)
-
-  path = "/data/MIMII/fan/id_04"
 
   X_train, X_test, y_train, y_test = files_train_test_split(path)
   # normal == 1
   # abnormal == 0
-  train_dataset = AcousticDatasetId(X_train, y_train, sampling_rate=16000, mono=True)
-  val_dataset = AcousticDatasetId(X_test, y_test, sampling_rate=16000, mono=True)
-  learning_rate = 0.001
-  epochs = 15
-  batch_size = 16
+  train_dataset = AcousticDatasetId(X_train, y_train, sampling_rate=sampling_rate, mono=mono)
+  val_dataset = AcousticDatasetId(X_test, y_test, sampling_rate=sampling_rate, mono=mono)
 
-  train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-  val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
-  train(train_loader, val_loader, learning_rate, model, epochs, device)
+  train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+  val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=shuffle)
+  train(train_loader, val_loader, lr, model, epochs, device)
 
 
 def train(train_loader, val_loader, learning_rate, model, epochs, device):
   criterion_reconstruction = nn.MSELoss()
   optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.98), eps=1e-9)
-
   for epoch in range(epochs):
     model.train()
     total_loss = 0.0
@@ -97,14 +88,30 @@ def validate(model, val_loader, device):
 
 
 if __name__ == "__main__":
+  device = torch.device("cuda")
+  learning_rates = [0.001]
+  epochs = [15]
+  ids = ["id_00", "id_02", "id_04", "id_06"]
+  machine_type = ["fan"]
+  batch_sizes = [16]
+  sampling_rate = 16000
+  mono = True
+  shuffle = True
 
-  model = build_transformer(mfcc_dim=20, d_model=128, nhead=2, num_encoder_layers=2, num_decoder_layers=2,
-                            dim_feedforward=1024, dropout=0.1, batch_first=True, device=device).to(device)
+  for lr in learning_rates:
+    for epoch in epochs:
+      for m_type in machine_type:
+        for path_id in ids:
+          for bs in batch_sizes:
+            path = Path("/data/MIMII", m_type, path_id)
+            print("MACHINE TYPE:", m_type, "and ID:", path_id)
+    
+            model = build_transformer(mfcc_dim=20, d_model=128, nhead=2, num_encoder_layers=2, num_decoder_layers=2,
+                                dim_feedforward=1024, dropout=0.1, batch_first=True, device=device).to(device)
 
-  path = ["/data/MIMII/fan/id_04"]
+            run_experiment(model=model, lr=lr, epochs=epoch, batch_size=bs, path=path,
+                          sampling_rate=sampling_rate, mono=mono, shuffle=shuffle, device=device)
+            
+            del model
 
-  learning_rate = [0.001, 0.0001]
-  epochs = [15, 20, 25]
-  batch_size = [16, 32]
-
-  run_experiment()
+  
